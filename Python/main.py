ed5befe324e7f177979db1d3db3d534bc1b40e93
Arthur Amorim <arthuramora.aa@gmail.com>
2020-04-24 10:15:38 -0300

    Código utilizando de pré-processamento (Data Standardization) antes de ser feita a aquisição das características (melhora significativa na acurácia). Comentei a segunda parte do código, que fazia a classificação de outra base de dados usando a rede pré-existente porque os resultados não foram satisfatórios o suficiente. Não testei a combinação do pré-processamento com o PCA porque creio que a maneira que estou utilizando do mesmo não é a ideal, pretendo implementa-lo de maneira a identificar as componentes onde está concentrada a maior porcentagem da variância na base de dados e utilizar os valores dessas PCs para treinar a rede.

--- Python/main.py
+++ Python/main.py
@@ -43,11 +43,20 @@ for i in range(0, len(sinal_div)):
 
 # Teste PCA
 
-#scaled_data = preprocessing.scale(feature_vec)
+#feature_vec = preprocessing.scale(feature_vec)
 
-#pca = PCA()
-#pca.fit(scaled_data)
-#pca_data = pca.score_samples(scaled_data)
+pca = PCA()
+pca.fit(feature_vec)
+pca_data = pca.transform(feature_vec)
+
+#per_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
+#labels = ['PC' + str(x) for x in range (1,len(per_var)+1)]
+
+#plt.bar(x=range(1,len(per_var)+1), height=per_var, tick_label=labels)
+#plt.ylabel('Percentage of Explained Varience')
+#plt.xlabel('Principal Component')
+#plt.title('Scree Plot')
+#plt.show()
 
 #saidas_trechos = np.asarray(saidas_trechos)
 
