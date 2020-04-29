2af779be2319b0ec7295add5a03ea78c789356e1
Arthur Amorim <arthuramora.aa@gmail.com>
2020-04-29 12:37:02 -0300

    Matriz de Confusão feita

--- Python/Modulos/RedeNeural.py
+++ Python/Modulos/RedeNeural.py
@@ -32,15 +32,14 @@ def treinamento_rna(data, labels):
 
     model.fit(X_train, y_train, validation_data=(X_test, y_test),batch_size=32, epochs=6000, shuffle=True, verbose=2)
 
-    # Plotando a matriz de confusão do treinamento/validação
+    # Matriz de confusão
 
-    #disp = plot_confusion_matrix(model, X_test, y_test,
-                                 #display_labels=("Convulsão", "Normal"),
-                                 #cmap="Blues",
-                                 #normalize="normalize")
+    rounded_predictions = predic_rna(model, data)
 
-    #print(disp.confusion_matrix)
-    #plt.show()
+    cm = confusion_matrix(labels, rounded_predictions)
+    cm_plot_labels = ['Normal', 'Epilepsia']
+
+    plot_confusion_matrix(cm, cm_plot_labels, title='Confusion Matrix')
 
     return model
 
@@ -51,3 +50,32 @@ def predic_rna(model, data):
     rounded_predictions = model.predict_classes(data, batch_size=32, verbose=0)
 
     return rounded_predictions
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matix', cmap=plt.cm.Blues):
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:,np.newaxis]
+        print("Normalized Confusion Matrix")
+    else:
+        print("Confusion Matrix")
+
+    print(cm)
+
+    thresh = cm.max()/2
+    for i,j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j,i,cm[i,j],
+                horizontalalignment="center",
+                color="white" if cm[i,j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
